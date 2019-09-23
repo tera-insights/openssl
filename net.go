3fd78dc63f69187c1b825a12da5f7817786984d7
Christopher Dudley <chris@github.chrisdudley.xyz>
2019-09-22 21:08:10 -0400

    net: support passing net.Dialer to Dial

--- net.go
+++ net.go
@@ -80,6 +80,22 @@ func Dial(network, addr string, ctx *Ctx, flags DialFlags) (*Conn, error) {
 	return DialSession(network, addr, ctx, flags, nil)
 }
 
+// DialWithDialer will connect to network/address and then wrap the corresponding
+// underlying connection with an OpenSSL client connection using context ctx.
+// If flags includes InsecureSkipHostVerification, the server certificate's
+// hostname will not be checked to match the hostname in addr. Otherwise, flags
+// should be 0.
+//
+// The specified dialer will be used to open the underlying TCP connection.
+//
+// Dial probably won't work for you unless you set a verify location or add
+// some certs to the certificate store of the client context you're using.
+// This library is not nice enough to use the system certificate store by
+// default for you yet.
+func DialWithDialer(dialer *net.Dialer, network, addr string, ctx *Ctx, flags DialFlags) (*Conn, error) {
+	return DialSessionWithDialer(dialer, network, addr, ctx, flags, nil)
+}
+
 // DialSession will connect to network/address and then wrap the corresponding
 // underlying connection with an OpenSSL client connection using context ctx.
 // If flags includes InsecureSkipHostVerification, the server certificate's
@@ -93,7 +109,26 @@ func Dial(network, addr string, ctx *Ctx, flags DialFlags) (*Conn, error) {
 //
 // If session is not nil it will be used to resume the tls state. The session
 // can be retrieved from the GetSession method on the Conn.
-func DialSession(network, addr string, ctx *Ctx, flags DialFlags,
+func DialSession(network, addr string, ctx *Ctx, flags DialFlags, session []byte) (*Conn, error) {
+	return DialSessionWithDialer(nil, network, addr, ctx, flags, session)
+}
+
+// DialSessionWithDialer will connect to network/address and then wrap the corresponding
+// underlying connection with an OpenSSL client connection using context ctx.
+// If flags includes InsecureSkipHostVerification, the server certificate's
+// hostname will not be checked to match the hostname in addr. Otherwise, flags
+// should be 0.
+//
+// The specified dialer will be used to open the underlying TCP connection.
+//
+// Dial probably won't work for you unless you set a verify location or add
+// some certs to the certificate store of the client context you're using.
+// This library is not nice enough to use the system certificate store by
+// default for you yet.
+//
+// If session is not nil it will be used to resume the tls state. The session
+// can be retrieved from the GetSession method on the Conn.
+func DialSessionWithDialer(dialer *net.Dialer, network, addr string, ctx *Ctx, flags DialFlags,
 	session []byte) (*Conn, error) {
 
 	host, _, err := net.SplitHostPort(addr)
@@ -108,7 +143,12 @@ func DialSession(network, addr string, ctx *Ctx, flags DialFlags,
 		}
 		// TODO: use operating system default certificate chain?
 	}
-	c, err := net.Dial(network, addr)
+	var c net.Conn
+	if dialer == nil {
+		c, err = net.Dial(network, addr)
+	} else {
+		c, err = dialer.Dial(network, addr)
+	}
 	if err != nil {
 		return nil, err
 	}
